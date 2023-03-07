e67d833fca4eb91db9f1ddbb11306806602618aa
michoball <myunghun0114@gmail.com>
2023-03-07 22:54:27 +0900

    refactor: Off no-param-reassign & @typescript-eslint/no-unused-vars & consistent-return rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,5 +21,8 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    'no-param-reassign': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'consistent-return': 'off',
   },
 }
