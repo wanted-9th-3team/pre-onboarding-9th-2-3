0ece0097187448ab54bd792865de60fe2a6d094d
cms <audtlr93@naver.com>
2023-03-07 19:21:27 +0900

    fix: ESLint 커밋 규칙에 의해, import 위치변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
+import { Navigate } from 'react-router-dom'
 import './App.css'
+import Main from './pages/Main'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Navigate to='/main' />} />
+        <Route path='/main' element={<Main />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
