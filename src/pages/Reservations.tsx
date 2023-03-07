84234ee70af8d4b9f12f373571fe73b4c0324a52
skyhanull <anky0425@naver.com>
2023-03-08 03:40:44 +0900

    feat:장바구니 페이지 세부내용 구현

---
+++ src/pages/Reservations.tsx
@@ -0,0 +1,62 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { RootState } from '../store/store'
+import { remove, patch } from '../store/slice/reservationSlice'
+
+function Reservation() {
+  const dispatch = useDispatch()
+  const todolist = useSelector((state: RootState) => state.reservations)
+  const [todoLists, setTodlLists] = useState(todolist)
+  const [pay, setPay] = useState(0)
+  const [amountOne, setAmountOne] = useState(0)
+
+  const payHandler = (one: any, tow: any) => {
+    const AAAA = {
+      id: one,
+      amount: tow,
+    }
+
+    dispatch(patch(AAAA))
+  }
+
+  useEffect(() => {
+    const A = todoLists.map(El => El.price * El.amount)
+    setPay(A.reduce((acc, cur) => acc + cur, 0))
+  }, [todoLists, dispatch])
+
+  return (
+    <div>
+      <div>
+        {todoLists.map(el => (
+          <div key={el.id}>
+            <span>{el.text}</span>
+            <span>{el.price}</span>
+
+            <button
+              type='button'
+              onClick={() => payHandler(el.id, el.amount + 1)}
+            >
+              +
+            </button>
+            <span>{amountOne}</span>
+            <button
+              type='button'
+              onClick={() => dispatch(patch(el.amount - 1))}
+            >
+              -
+            </button>
+            <button type='button' onClick={() => dispatch(remove(el.id))}>
+              삭제
+            </button>
+          </div>
+        ))}
+      </div>
+      <Link to='/main'>
+        <button type='button'>돌아가기</button>
+      </Link>
+      {pay}
+    </div>
+  )
+}
+export default Reservation
