36ec7c19a4f06ce484b70aec8ca9297971ca6c5e
cms <audtlr93@naver.com>
2023-03-07 22:49:26 +0900

    feat: main 파일에 리덕스 스토어 프로바이더 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
 import './App.css'
 import Main from './pages/Main'
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/main' />} />
-        <Route path='/main' element={<Main />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to='/main' />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
