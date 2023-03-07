b70a723d886a0653597fe93b0f66de7aabe71e03
michoball <myunghun0114@gmail.com>
2023-03-07 22:48:32 +0900

    refactor: setList fetch code and dispatch it into setTravelLists action

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Main from './pages/Main'
 import NotFound from './pages/NotFound'
 import './App.css'
+import getTravelInfo from './api/Travel-api'
+import { useAppDispatch } from './store/store'
+import { setTravelLists } from './store/travel/travelSlice'
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const getList = async () => {
+      const data = await getTravelInfo()
+      if (data) dispatch(setTravelLists(data))
+    }
+    getList()
+  }, [dispatch])
+
   return (
     <BrowserRouter>
       <Routes>
