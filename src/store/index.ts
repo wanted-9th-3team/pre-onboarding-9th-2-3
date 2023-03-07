36fb0ca9777c06f1b55722707bcb96e66857e21a
cms <audtlr93@naver.com>
2023-03-07 23:05:48 +0900

    feat: 스토어에 모달용 슬라이스 생성
    모달을 띄우기 위한 정보를 담을
    슬라이스를 생성

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './product-slice'
+import modalSlice from './modal-slice'
 
-export const store = configureStore({ reducer: { productsSlice } })
+export const store = configureStore({ reducer: { productsSlice, modalSlice } })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
---
+++ src/store/modal-slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { TypeProduct } from '../type'
+
+const initialState: { modalInfo: TypeProduct | null } = {
+  modalInfo: null,
+}
+
+const modalSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {
+    insertModal(state, action: PayloadAction<TypeProduct>) {
+      state.modalInfo = action.payload
+    },
+    deleteModal(state) {
+      state.modalInfo = null
+    },
+  },
+})
+
+export const modalActions = modalSlice.actions
+
+export default modalSlice.reducer
