779700c1538db45cd69df51359a3de3dd02342ab
cms <audtlr93@naver.com>
2023-03-08 00:05:50 +0900

    feat: 메인페이지 인포메이션 컴포넌트, modal 정보 받아, 모달로 넣어줄 data 삽입
    인포메이션 컴포넌트에서
    메인페이지에서 모달로 넘겨줄 데이터를
    스토어에서 받아옴

--- src/components/main/Information.tsx
+++ src/components/main/Information.tsx
@@ -2,10 +2,14 @@ import { useSelector } from 'react-redux'
 import { Box } from '@chakra-ui/react'
 import { RootState } from '../../store'
 import CardProduct from '../ui/card/Product-card'
+import ModalProduct from '../ui/modal/Product-modal'
 
 function Information() {
   const productsInfo = useSelector(
     (state: RootState) => state.productsSlice.allProducts
+  )
+  const modalInfo = useSelector(
+    (state: RootState) => state.modalSlice.modalInfo
   )
 
   const productsJSX = productsInfo.map(item => (
@@ -14,6 +18,7 @@ function Information() {
   return (
     <Box display='flex' alignItems='center' flexWrap='wrap'>
       {productsJSX}
+      {modalInfo && <ModalProduct modalInfo={modalInfo} />}
     </Box>
   )
 }
