39c621e6d471c7457ff9df01b1e621d58019a016
skyhanull <anky0425@naver.com>
2023-03-07 23:46:52 +0900

    feat: 모달컴포넌트에 props받아오는 것을 구현

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,5 +1,25 @@
-function Modal() {
-  return <div>modal추가</div>
+import { useState } from 'react'
+import Mock_Data from './data/mock_data.json'
+
+interface Iidx {
+  idx: number
+}
+
+interface IMockData {
+  idx: number
+  name: string
+  mainImage: string
+  description: string
+  spaceCategory: string
+  price: number
+  maximumPurchases: number
+  registrationDate: Date
+}
+
+function Modal({ modalData }: any) {
+  // const [data, setData] = useState(Mock_Data)
+  console.log(modalData[0].idx)
+  return <div>{modalData[0].idx}</div>
 }
 
 export default Modal
