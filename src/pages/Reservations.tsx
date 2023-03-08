ed2f485084c39f22620796ad43785478612d20ae
cms <audtlr93@naver.com>
2023-03-08 17:32:30 +0900

    feat: 예약 페이지 추가

---
+++ src/pages/Reservations.tsx
@@ -0,0 +1,13 @@
+import ReservationCalc from '../components/reservations/Reservation-calc'
+import ReservationInfo from '../components/reservations/Reservation-info'
+
+function Reservations() {
+  return (
+    <>
+      <ReservationInfo />
+      <ReservationCalc />
+    </>
+  )
+}
+
+export default Reservations
