b9d6c6dea9be84029a718ba8ab5f2f66aafd61ab
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-03-08 11:45:09 +0900

    feat : 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+
+import Main from './pages/Main'
 import NotFound from './pages/NotFound'
+import Reservations from './pages/Reservations'
+
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/main' element={<Main />} />
+        <Route path='/reservations' element={<Reservations />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
