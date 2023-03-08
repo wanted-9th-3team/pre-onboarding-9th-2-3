9d042c8d5a85dddaa435d360f81d23f254cbd323
cms <audtlr93@naver.com>
2023-03-08 15:42:31 +0900

    feat: 스토어에 필터 슬라이스 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsSlice from './product-slice'
 import modalSlice from './modal-slice'
+import filterSlice from './filter-slice'
 
-export const store = configureStore({ reducer: { productsSlice, modalSlice } })
+export const store = configureStore({
+  reducer: { productsSlice, modalSlice, filterSlice },
+})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
