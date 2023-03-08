2694672ea09b17fa1775ec9acceff146f0e70f70
cms <audtlr93@naver.com>
2023-03-08 13:16:46 +0900

    feat: 필터링할 컴포넌트 생성
    필터링을 위해 챠크라 UI 의 range 를 이용한 가격 필터 UI
    
    버튼 그룹 을 이용한 공간 필터 UI 생성

---
+++ src/components/ui/filter/ui-filter.tsx
@@ -0,0 +1,101 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import {
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Box,
+  Text,
+  ButtonGroup,
+  Button,
+} from '@chakra-ui/react'
+import { RootState } from '../../../store'
+
+function FilterUI() {
+  const productsInfo = useSelector(
+    (state: RootState) => state.productsSlice.allProducts
+  )
+  const [priceFilter, setPriceFilter] = useState<string[] | undefined>()
+  const [spaceFilter, setSpaceFilter] = useState<string[]>([])
+
+  const deleteDuplicationSpaceHandler = () => {
+    const onlySpace = productsInfo.map(item => item.spaceCategory)
+    return new Set(onlySpace)
+  }
+
+  const spaceFilterHandler = (space: string) => {
+    const isExist = spaceFilter.find(item => item === space)
+
+    if (isExist) {
+      setSpaceFilter(prev => prev.filter(item => item !== isExist))
+    } else {
+      setSpaceFilter(prev => [...prev, space])
+    }
+  }
+
+  const deleteDuplicationSpace = [...deleteDuplicationSpaceHandler()]
+
+  const sumbitHandler = () => {}
+
+  return (
+    <Box className='filter'>
+      <Box className='filter__price'>
+        <Text fontSize='xl' as='b' display='block'>
+          가격 필터
+        </Text>
+        <RangeSlider
+          width='30rem'
+          defaultValue={[0, 5000]}
+          min={0}
+          max={100000}
+          step={5000}
+          onChange={e => setPriceFilter(e.map(item => item.toString()))}
+        >
+          <RangeSliderTrack bg='blackAlpha.400'>
+            <RangeSliderFilledTrack bg='blue.300' />
+          </RangeSliderTrack>
+          <RangeSliderThumb boxSize={6} index={0} />
+          <RangeSliderThumb boxSize={6} index={1} />
+        </RangeSlider>
+        <Box className='range__value' display='flex' justifyContent='center'>
+          <Box m={5}>
+            <Text>최소 가격</Text>
+            <Text>{priceFilter !== undefined ? priceFilter[0] : '미설정'}</Text>
+          </Box>
+          <Box m={5}>
+            <Text>최대 가격</Text>
+            <Text>{priceFilter !== undefined ? priceFilter[1] : '미설정'}</Text>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box className='filter__space'>
+        <Text fontSize='xl' as='b' display='block'>
+          공간 필터
+        </Text>
+        <ButtonGroup spacing={3}>
+          {deleteDuplicationSpace.map(item => (
+            <Button
+              key={item}
+              onClick={() => spaceFilterHandler(item)}
+              className={
+                spaceFilter.find(it => it === item) && 'button__active'
+              }
+            >
+              {item}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </Box>
+
+      <Box className='filter__buttons' m={5}>
+        <Button type='button' size='md' colorScheme='teal'>
+          적용
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default FilterUI
