d718f22525b15a5bd4cf46bbe1b07440052b1d52
michoball <myunghun0114@gmail.com>
2023-03-08 14:55:23 +0900

    feat: 장바구니페이지 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 import getTravelInfo from './api/travel-api'
 import { useAppDispatch } from './store/store'
 import { setTravelLists } from './store/travel/travelSlice'
+import Reservations from './pages/Reservations'
+import Navigation from './pages/Navigation'
 
 function App() {
   const dispatch = useAppDispatch()
@@ -21,8 +23,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Navigate to='/main' />} />
-        <Route path='/main' element={<Main />} />
+        <Route path='/' element={<Navigation />}>
+          <Route path='/' element={<Navigate to='/main' />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/reservations' element={<Reservations />} />
+        </Route>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
