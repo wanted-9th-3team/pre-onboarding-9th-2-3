9c1093dadc3896cef8b5f6012f65e63a9fce73db
michoball <myunghun0114@gmail.com>
2023-03-09 02:48:41 +0900

    refactor: Change file name

---
+++ src/api/travelApi.tsx
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import ITravelInfo from './TravelDTO'
+
+const getTravelInfo = async () => {
+  const response = await axios('src/data/mock_data.json')
+
+  if (response) {
+    const { data } = response
+    return data as ITravelInfo[]
+  }
+  return null
+}
+
+export default getTravelInfo
