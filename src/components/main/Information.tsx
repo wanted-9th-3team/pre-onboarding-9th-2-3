259d09a15aba0ac2bc13a7f60ec9796b5ac7b606
cms <audtlr93@naver.com>
2023-03-08 15:40:16 +0900

    feat: 필터된 상품들을 UI 에 표시
    구현해놓은 필터링 로직을 사용하여
    조건부(space filter, price filter) 에 따라
    JSX 코드를 동적으로 반환합니다.

--- src/components/main/Information.tsx
+++ src/components/main/Information.tsx
@@ -3,6 +3,8 @@ import { Box } from '@chakra-ui/react'
 import { RootState } from '../../store'
 import CardProduct from '../ui/card/Product-card'
 import ModalProduct from '../ui/modal/Product-modal'
+import FilterUI from '../ui/filter/ui-filter'
+import { filterBySpace, filterByPrice } from '../../util/products-filtering'
 
 function Information() {
   const productsInfo = useSelector(
@@ -12,13 +14,42 @@ function Information() {
     (state: RootState) => state.modalSlice.modalInfo
   )
 
-  const productsJSX = productsInfo.map(item => (
+  const filterInfo = useSelector((state: RootState) => state.filterSlice.filter)
+
+  let productsJSX = productsInfo.map(item => (
     <CardProduct key={item.idx} product={item} />
   ))
+
+  if (filterInfo?.maxPrice && filterInfo?.space) {
+    const filteredByPrice = filterByPrice(productsInfo, filterInfo)
+    const filteredBySpace = filterBySpace(filteredByPrice, filterInfo!)
+    productsJSX = filteredBySpace.map(item => (
+      <CardProduct key={item.idx} product={item} />
+    ))
+  } else if (filterInfo?.maxPrice) {
+    const filteredByPrice = filterByPrice(productsInfo, filterInfo)
+    productsJSX = filteredByPrice.map(item => (
+      <CardProduct key={item.idx} product={item} />
+    ))
+  } else if (filterInfo?.space) {
+    const filteredBySpace = filterBySpace(productsInfo, filterInfo!)
+    productsJSX = filteredBySpace.map(item => (
+      <CardProduct key={item.idx} product={item} />
+    ))
+  }
+
   return (
-    <Box display='flex' alignItems='center' flexWrap='wrap'>
-      {productsJSX}
+    <Box>
+      <FilterUI />
       {modalInfo && <ModalProduct modalInfo={modalInfo} />}
+      <Box
+        className='main__lists'
+        display='flex'
+        alignItems='center'
+        flexWrap='wrap'
+      >
+        {productsJSX}
+      </Box>
     </Box>
   )
 }
