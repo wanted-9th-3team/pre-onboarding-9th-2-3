2b66c7e35381db151cdff7768c3169138b0484fa
skyhanull <anky0425@naver.com>
2023-03-08 17:45:18 +0900

    refactor: css추가

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,6 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import {
+  Card,
+  Box,
+  CardBody,
+  CardFooter,
+  Stack,
+  Image,
+  Heading,
+  Button,
+  Text,
+} from '@chakra-ui/react'
 import { RootState } from '../store/store'
 import { remove, patch } from '../store/slice/reservationSlice'
 
@@ -23,38 +34,80 @@ function Reservation() {
   }, [todolist])
 
   return (
-    <div>
-      <div>
-        {todolist.map(el => (
-          <div key={el.id}>
-            <span>{el.text}</span>
-            <span>{el.price}</span>
-
-            <button
-              type='button'
-              onClick={() => payHandler(el.id, el.amount + 1)}
-            >
-              +
-            </button>
-            {el.amount}
-            <button
-              type='button'
-              onClick={() => payHandler(el.id, el.amount - 1)}
-            >
-              -
-            </button>
-
-            <button type='button' onClick={() => dispatch(remove(el.id))}>
-              삭제
-            </button>
-          </div>
-        ))}
-      </div>
-      <Link to='/main'>
-        <button type='button'>돌아가기</button>
-      </Link>
+    <Stack direction='row'>
       {pay}
-    </div>
+
+      <Card
+        direction={{ base: 'column', sm: 'row' }}
+        overflow='hidden'
+        variant='outline'
+        bg='gray.200'
+      >
+        <Box w='50vw' margin='10px'>
+          {todolist.map(el => (
+            <Box key={el.id} border='1px' borderColor='gray.600' margin='10px'>
+              <Image
+                objectFit='cover'
+                maxW={{ base: '50%' }}
+                src={el.mainImage}
+                alt=''
+                align='center'
+              />
+              <Stack>
+                <CardBody>
+                  <Heading size='md'>{el.text}</Heading>
+
+                  <Text>
+                    <Text>{el.id}</Text>
+                    <Stack direction='row'>
+                      <Button
+                        type='button'
+                        onClick={() => payHandler(el.id, el.amount + 1)}
+                      >
+                        +
+                      </Button>
+                      <Text>{el.amount}</Text>
+                      <Button
+                        type='button'
+                        onClick={() => payHandler(el.id, el.amount - 1)}
+                      >
+                        -
+                      </Button>
+                    </Stack>
+                    <Text>가격:{el.price * el.amount}</Text>
+                  </Text>
+                </CardBody>
+
+                <CardFooter>
+                  <Button
+                    variant='solid'
+                    colorScheme='blue'
+                    onClick={() => dispatch(remove(el.id))}
+                  >
+                    삭제
+                  </Button>
+                </CardFooter>
+              </Stack>
+            </Box>
+          ))}
+        </Box>
+      </Card>
+      <Box
+        bg='blue.100'
+        w='100%'
+        h='200px'
+        display='flex'
+        alignItems='center'
+        justifyContent='center'
+        flexDirection='column'
+        flexWrap='wrap'
+      >
+        <Text> 총 가격: {pay}</Text>
+        <Link to='/main'>
+          <Button type='button'>돌아가기</Button>
+        </Link>
+      </Box>
+    </Stack>
   )
 }
 export default Reservation
