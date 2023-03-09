86402a1339df805be1f8d3f749df83320b571924
skyhanull <anky0425@naver.com>
2023-03-09 20:48:16 +0900

    feat: ui부분 추가

--- src/components/trip/TripCard.tsx
+++ src/components/trip/TripCard.tsx
@@ -49,7 +49,59 @@ function TripCard({ travelInfo }: ITripCardProps) {
     }
   }
 
-  return <div />
+  return (
+    <>
+      {/* <CardModal isOpen={isOpen} onClose={onClose} idx={idx} /> */}
+      <Card
+        direction={{ base: 'row' }}
+        overflow='hidden'
+        variant='outline'
+        width='480px'
+        height='120px'
+        justify='space-between'
+      >
+        <Image
+          objectFit='cover'
+          maxW={{ base: '100%', sm: '100px' }}
+          align='center'
+          src={mainImage}
+          alt={name}
+          borderRadius='lg'
+        />
+
+        <CardBody>
+          <Text size='sm' fontSize='14px' fontWeight='bold'>
+            <Badge colorScheme='green' mr='2'>
+              {idx}
+            </Badge>
+            {name}
+          </Text>
+          <Stack direction='row' justify='center' align='center'>
+            <Text py='2'>위치 : {spaceCategory}</Text>
+            <Text color='blue.600' fontSize='md'>
+              ${price}
+            </Text>
+          </Stack>
+          <ButtonGroup spacing='2' size='xs'>
+            <Button
+              variant='solid'
+              colorScheme='blue'
+              onClick={addToCartHandler}
+            >
+              예약하기
+            </Button>
+            <Button
+              variant='solid'
+              colorScheme='teal'
+              onClick={openModalHandler}
+            >
+              상세정보
+            </Button>
+          </ButtonGroup>
+        </CardBody>
+      </Card>
+    </>
+  )
 }
 
 export default TripCard
