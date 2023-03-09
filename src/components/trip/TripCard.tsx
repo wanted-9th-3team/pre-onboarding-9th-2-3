5672b1d8866e8dd6555dbe436ac3e528ad1a1994
skyhanull <anky0425@naver.com>
2023-03-09 21:32:13 +0900

    feat: import및 인수의 type지정

--- src/components/trip/TripCard.tsx
+++ src/components/trip/TripCard.tsx
@@ -1,4 +1,23 @@
-function TripCard() {
+import {
+  Badge,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
+import { ITripInfo } from '../../Type'
+
+interface ITripCardProps {
+  travelInfo: ITripInfo
+}
+
+function TripCard({ travelInfo }: ITripCardProps) {
   return <div />
 }
 
