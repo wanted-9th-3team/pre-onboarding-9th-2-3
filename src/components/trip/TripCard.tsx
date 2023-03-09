868f33bd0cd418bf6ae508c3aa9638fbed3112da
skyhanull <anky0425@naver.com>
2023-03-09 20:43:00 +0900

    feat: css에 필요한 import 및 dispatch추가

--- src/components/trip/TripCard.tsx
+++ src/components/trip/TripCard.tsx
@@ -1,10 +1,28 @@
+import {
+  Badge,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  Image,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
 import { ITripInfo } from '../../Type'
+import { useAppDispatch } from '../../store/store'
 
 interface ITripCardProps {
   travelInfo: ITripInfo
 }
 
 function TripCard({ travelInfo }: ITripCardProps) {
+  const { idx, name, mainImage, price, spaceCategory } = travelInfo
+  // const selectedCartItem = useSelector(selectCartItems)
+  const toast = useToast()
+  const dispatch = useAppDispatch()
   return <div />
 }
 
