8317701508b502c0a7a6ea0c72029d6833164625
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-03-09 21:56:51 +0900

    feat : tripSelector에 필터 추가

--- src/store/trip/tripSelector.ts
+++ src/store/trip/tripSelector.ts
@@ -13,3 +13,29 @@ export const selectSelectedTripList = createSelector(
   [selectCartReducer],
   trip => trip.selectedtripList
 )
+
+export const selectPriceRange = createSelector(
+  [selectCartReducer],
+  trip => trip.priceRange
+)
+
+export const searchTravelSpaceLists = createSelector(
+  [selectCartReducer],
+  trips => {
+    const spaceList = trips.tripList.map(trip => trip.spaceCategory)
+    return [...new Set(spaceList)]
+  }
+)
+
+export const searchedTripLists = createSelector([selectCartReducer], travel => {
+  const { searchCategory, tripList } = travel
+  const { priceRange, selectSpace } = searchCategory
+
+  const spacesortedTripList = tripList.filter(trip => {
+    return selectSpace.length ? selectSpace.includes(trip.spaceCategory) : trip
+  })
+
+  return spacesortedTripList.filter(
+    list => list.price >= priceRange[0] && list.price <= priceRange[1]
+  )
+})
