44187c8805a023aca564cf31ebb738761bb28ccd
michoball <myunghun0114@gmail.com>
2023-03-09 18:48:32 +0900

    feat: UseEffect 로 data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import { useAppDispatch } from './store/store'
+import { getTrip } from './store/trip/tripSlice'
 import './App.css'
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getTrip())
+  }, [dispatch])
+
   return (
     <BrowserRouter>
       <Routes>
