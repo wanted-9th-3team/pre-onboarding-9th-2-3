691444972fb99e67da7455e1edb79000ac5fdc68
michoball <myunghun0114@gmail.com>
2023-03-09 18:34:22 +0900

    feat: 기본 store 셋팅

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { useDispatch } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+const rootReducer = combineReducers({})
+
+export const store = configureStore({
+  reducer: rootReducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
