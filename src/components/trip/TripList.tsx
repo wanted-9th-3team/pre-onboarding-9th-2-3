8925a2c72a22de11c34c27729b6a97cf6e2a7391
skyhanull <anky0425@naver.com>
2023-03-09 20:36:23 +0900

    feat: style과 card로 list를 map돌리는 것을 추가

--- src/components/trip/TripList.tsx
+++ src/components/trip/TripList.tsx
@@ -1,10 +1,24 @@
 import { useSelector } from 'react-redux'
 import { searchedTripLists } from '../../store/trip/tripSelector'
+import TripCard from './TripCard'
 
 function TripList() {
   const searchedTripList = useSelector(searchedTripLists)
 
-  return <div />
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        gap: '10px',
+      }}
+    >
+      {searchedTripList.map(travel => {
+        return <TripCard key={travel.idx} travelInfo={travel} />
+      })}
+    </div>
+  )
 }
 
 export default TripList
