dccd131f6926d8ff199aecc5e5f925dab3cd4679
skyhanull <anky0425@naver.com>
2023-03-09 20:57:44 +0900

    refactor: mainpage 코드 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,16 @@
+import { Center, Stack } from '@chakra-ui/react'
+import TripList from '../components/trip/TripList'
+
 function MainPage() {
-  return <div />
+  return (
+    <Stack align='center'>
+      <Center h='100px'>
+        <h1 style={{ fontSize: '30px', fontWeight: 'bold' }}>여행 상품 정보</h1>
+      </Center>
+
+      <TripList />
+    </Stack>
+  )
 }
 
 export default MainPage
