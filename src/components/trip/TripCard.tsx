c5e7acdcab256c59b3768a21f43825b8cbc1e9f4
skyhanull <anky0425@naver.com>
2023-03-09 21:34:25 +0900

    feat: modalhandler및 import추가

--- src/components/trip/TripCard.tsx
+++ src/components/trip/TripCard.tsx
@@ -12,12 +12,27 @@ import {
 } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { ITripInfo } from '../../Type'
+// import { addCartList } from '../../store/cart/cartSlice'
+import { useAppDispatch } from '../../store/store'
+import { setSelectedtripList } from '../../store/trip/tripSlice'
+import CardModal from '../modal/CardModal'
+// import { selectCartItems } from '../../store/cart/cartSelector'
 
 interface ITripCardProps {
   travelInfo: ITripInfo
 }
 
 function TripCard({ travelInfo }: ITripCardProps) {
+  const { idx, name, mainImage, price, spaceCategory } = travelInfo
+  // const selectedCartItem = useSelector(selectCartItems)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const toast = useToast()
+  const dispatch = useAppDispatch()
+
+  const openModalHandler = () => {
+    dispatch(setSelectedtripList(idx))
+    onOpen()
+  }
   return <div />
 }
 
