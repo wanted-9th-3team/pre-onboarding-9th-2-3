75eab6b214460f4d4fce84cd904b5d389df3e915
DESKTOP-5RRNCP0\Yuri <chsem145@naver.com>
2023-03-10 20:35:45 +0900

    feat : public directory 변경

--- src/api/tripApi.tsx
+++ src/api/tripApi.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getTripInfo = async () => {
-  const response = await axios('src/data/mock_data.json')
+  const response = await axios('mock_data.json')
 
   if (response.status === 200) {
     const { data } = response
